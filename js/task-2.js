0afebdbea78645a85da56024f0d03a5076076928
katya_nad <daduda4656@gmail.comexit>
2026-05-21 23:18:56 +0300

    my second js-hw

--- js/task-2.js
+++ js/task-2.js
@@ -4,7 +4,6 @@ function formatMessage(message, maxLength) {
     }
     return message.slice(0, maxLength) + "...";
 }
-}
 
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
